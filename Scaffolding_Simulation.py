164080964e56f6463ae73ae9c36f3a194d4fcf32
Larry Grullon-Polanco <larrygrpolanco@gmail.com>
2024-03-29 16:07:14 -0400

    Slight fixes
    More complicated that I though

--- Scaffolding_Simulation.py
+++ Scaffolding_Simulation.py
@@ -1,11 +1,10 @@
 import streamlit as st
 import openai
 
-# Ideally, store the API key in a secure location or environment variable
-OPENAI_API_KEY = st.secrets[OPENAI_API_KEY]
+client = openai.OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
-openai.api_key = OPENAI_API_KEY
-
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-4-turbo-preview"
 
 def generate_response(messages):
     try:
@@ -32,3 +31,40 @@ extension_questions_dict = {
     "Territorial": "Do you feel territorial when someone touches the things on your desk?Tell us why?",
     "Tisible": "Which fruit does not have seeds visible on the inside when you cut it in half? An orange, an apple or a strawberry.",
 }
+
+st.title("Scaffolding Practice Simulation")
+
+col1, col2 = st.columns(2)
+# UI for selecting vocabulary word and scaffold direction
+chosen_vocab = col1.selectbox(
+    "Vocabulary word:", options=list(extension_questions_dict.keys())
+)
+scaffold_direction = col2.selectbox(
+    "Scaffold Direction:", ["Random", "Upward", "Downward"]
+)
+
+st.markdown(f"Copy Question:  \n**{extension_questions_dict[chosen_vocab]}**")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input(extension_questions_dict[chosen_vocab]):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": m["role"], "content": m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append({"role": "assistant", "content": response})
